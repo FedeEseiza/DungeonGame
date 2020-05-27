11a754e0325b87cc73308b4a9dcc88c656bff5f7
Marcos9098 <marcos909890989098@gmail.com>
2020-05-27 02:22:00 -0300

    Arreglada clase Bonus, ahora abstracta
    se agrego un metodo abstracto "consume"

--- DungeonGame/src/DungeonAndLevels/Bonus.java
+++ DungeonGame/src/DungeonAndLevels/Bonus.java
@@ -1,7 +1,9 @@
 package DungeonAndLevels;
 
 public abstract class Bonus {
+	protected int amount;
 	
 	public abstract void myFateWithThisLevel(Character player);
+	public abstract void consume(Character player);
 	
 }
